49318d89e9e0c79aeb92600f9a697eeeb728e9e9
Usevalad Prastsiakou <folgo1006@gmail.com>
2024-12-07 20:16:26 +0300

    fix

--- src/models.py
+++ src/models.py
@@ -1,6 +1,7 @@
 from typing import List
 
 from sqlalchemy.orm import mapped_column, Mapped, relationship
+from sqlalchemy import ForeignKey
 from src.utils.db import Base, session
 
 
@@ -8,10 +9,16 @@ class Users(Base):
     __tablename__ = 'users'
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    tg_id: Mapped[int] = mapped_column(nullable=False)
+    tg_id: Mapped[int] = mapped_column(nullable=False, unique=True)
     role: Mapped[str] = mapped_column(nullable=False)
     chat: Mapped[str] = mapped_column(nullable=False)
-    messages: Mapped[List["Messages"]] = relationship(back_populates="user")
+
+    sent_messages: Mapped[List["Messages"]] = relationship(
+        "Messages", back_populates="from_user", foreign_keys="Messages.from_user_id"
+    )
+    received_messages: Mapped[List["Messages"]] = relationship(
+        "Messages", back_populates="to_user", foreign_keys="Messages.to_user_id"
+    )
 
     def __repr__(self):
         return f"User({self.role})"
@@ -25,9 +32,16 @@ class Messages(Base):
     __tablename__ = 'messages'
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    text: Mapped[int] = mapped_column()
-    to_user: Mapped["Users"] = relationship(back_populates="messages")
-    from_user: Mapped["Users"] = relationship(back_populates="messages")
+    text: Mapped[str] = mapped_column(nullable=False)
+
+    from_user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
+    to_user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
+    from_user: Mapped["Users"] = relationship(
+        "Users", back_populates="sent_messages", foreign_keys=[from_user_id]
+    )
+    to_user: Mapped["Users"] = relationship(
+        "Users", back_populates="received_messages", foreign_keys=[to_user_id]
+    )
 
     def __repr__(self):
-        return f"Message({self.from_user.role})"
+        return f"Message(from={self.from_user.role}, to={self.to_user.role})"
