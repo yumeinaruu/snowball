2bc81de04f9328640368946a964a898b49d1e33b
Usevalad Prastsiakou <folgo1006@gmail.com>
2024-12-07 15:46:23 +0300

    Fix

---
+++ src/models.py
@@ -0,0 +1,20 @@
+from sqlalchemy.orm import mapped_column, Mapped, relationship
+from src.utils.db import Base
+
+
+class Users(Base):
+    __tablename__ = 'users'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    tg_id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(unique=True, nullable=False)
+    messages: Mapped["Messages"] = relationship("Messages", back_populates="user")
+
+
+class Messages(Base):
+    __tablename__ = 'messages'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    text: Mapped[int] = mapped_column()
+    to_user_id: Mapped["Users"] = mapped_column()
+    user_id: Mapped["Users"] = mapped_column()
--- src/snowball/handlers.py
+++ src/snowball/handlers.py
@@ -7,4 +7,20 @@ from .routers import snowball_router
 async def start(
         message: types.Message
 ):
-    await message.answer("Hi!")
+    msg = """
+    """
+    await message.answer(msg)
+
+
+@snowball_router.message(Command("register"))
+async def register(
+        message: types.Message
+):
+    await message.answer(str(message.from_user))
+
+
+@snowball_router.message(Command("send"))
+async def send(
+        message: types.Message
+):
+    await message.answer(str(message.from_user))
