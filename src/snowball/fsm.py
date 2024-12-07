aba16511084464b775d48c07391d5c0f39f63407
yumeinaruu <stanislav.lisovoj@krainet.by>
2024-12-07 16:38:30 +0300

    fix

--- src/snowball/fsm.py
+++ src/snowball/fsm.py
@@ -1,6 +1,12 @@
+from aiogram.fsm.state import StatesGroup, State
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 
 def make_row_keyboard(items: list[str]) -> ReplyKeyboardMarkup:
     row = [KeyboardButton(text=item) for item in items]
     return ReplyKeyboardMarkup(keyboard=[row], resize_keyboard=True)
+
+
+class Register(StatesGroup):
+    choosing_food_name = State()
+    choosing_food_size = State()
